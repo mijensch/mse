929368d64844c6f11e68e4be3c7a365823b9e186
mijensch <michaela.jensch@stud.th-luebeck.de>
2021-12-03 20:05:40 +0100

    Create WeihnachtsbaumGenerator.java

---
+++ WeihnachtsbaumGenerator.java
@@ -0,0 +1,39 @@
+package moderneSoftwareentwicklung;
+
+import java.util.Scanner; 
+
+/**
+ * Generiert nach Angabe der gewünschen Höhe einen Weihnachtsbaum.
+ * @author: Michaela 
+ * @version: 1.0
+ */
+
+public class WeihnachtsbaumGenerator {
+  public static void main(String [] args) {
+      Scanner in = new Scanner(System.in);
+      System.out.println("Geben Sie die Höhe an:");
+
+      int hoehe = in.nextInt();
+      if (hoehe <= 0) {
+        System.out.println("Bitte geben Sie eine Zahl ein, die größer ist als 0");
+      }
+
+      for (int i = 0; i < hoehe; i++) {
+      for (int k = 0; k < hoehe - 1 - i; k++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i * 2 + 1; j++) { 
+        System.out.print("*");
+      }
+        System.out.println();
+      }
+
+      int i;
+      for (i = 1; i < hoehe; i++) {
+        System.out.print(" ");	
+      }
+      System.out.println("I");
+      in.close();
+   }
+}
+
